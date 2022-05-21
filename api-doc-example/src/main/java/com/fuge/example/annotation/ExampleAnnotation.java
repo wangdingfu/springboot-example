9049c5e2eead381415e38f31ce45fafd15509307
wangdingfu <wangdingfu1024@163.com>
2022-05-21 22:36:10 +0800

    新增多维数组测试生成api

--- api-doc-example/src/main/java/com/fuge/example/annotation/ExampleAnnotation.java
+++ api-doc-example/src/main/java/com/fuge/example/annotation/ExampleAnnotation.java
@@ -1,13 +1,21 @@
 package com.fuge.example.annotation;
 
+import com.fuge.example.constants.ExampleConstants;
 import com.fuge.example.enumtype.ExampleEnum;
+import com.fuge.example.pojo.vo.ExampleDetailVO;
 
 import java.lang.annotation.*;
+import java.math.BigDecimal;
 
 @Target(ElementType.PARAMETER)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 public @interface ExampleAnnotation {
+    int big() default 0;
+
+
+
+    String string() default "";
 
     ExampleEnum value() default ExampleEnum.EXAMPLE;
 

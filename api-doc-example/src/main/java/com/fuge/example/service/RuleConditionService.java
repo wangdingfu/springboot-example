7d9931f5253b6e311447d12d66ca07a7e16ab5ab
wangdingfu <fuge@douyu.tv>
2022-08-09 19:43:12 +0800

    1

--- api-doc-example/src/main/java/com/fuge/example/service/RuleConditionService.java
+++ api-doc-example/src/main/java/com/fuge/example/service/RuleConditionService.java
@@ -15,7 +15,19 @@ import java.util.List;
 public interface RuleConditionService {
 
 
+    /**
+     * 保存规定则条件
+     *
+     * @param ruleConditionDTO 规则条件参数
+     */
     void saveCondition(RuleConditionDTO ruleConditionDTO);
 
+
+    /**
+     * 根据规则ID获取规则条件集合
+     *
+     * @param ruleId 规则ID
+     * @return 规则条件集合
+     */
     List<RuleConditionVO> findByRuleId(String ruleId);
 }
